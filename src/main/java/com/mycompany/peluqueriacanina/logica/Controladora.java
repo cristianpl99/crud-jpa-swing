c15122c8711a09dbdffaaf79ee25d61419910c8a
Cristian Pereyra <cristianpl99@gmail.com>
2023-01-27 00:24:24 -0300

    First

---
+++ src/main/java/com/mycompany/peluqueriacanina/logica/Controladora.java
@@ -0,0 +1,33 @@
+
+package com.mycompany.peluqueriacanina.logica;
+
+import com.mycompany.peluqueriacanina.persistencia.ControladoraPersistencia;
+
+
+public class Controladora {
+    
+    ControladoraPersistencia controlPersis = new ControladoraPersistencia();
+
+    public void guardar(String nombreMasco, String raza, String color, String observaciones, String nombreDuenio, 
+                                    String celDuenio, String alergico, String atEsp) {
+        
+        Duenio duenio = new Duenio();
+        
+        duenio.setNombre(nombreDuenio);
+        duenio.setCelDuenio(celDuenio);
+        
+        Mascota masco = new Mascota();
+        
+        masco.setNombre(nombreMasco);
+        masco.setRaza(raza);
+        masco.setColor(color);
+        masco.setAlergico(alergico);
+        masco.setAtencion_especial(atEsp);    
+       masco.setUnDuenio(duenio);
+       masco.setObservaciones(observaciones);
+       
+       controlPersis.guardar(duenio, masco);
+    }
+
+    
+}
---
+++ src/main/java/com/mycompany/peluqueriacanina/persistencia/ControladoraPersistencia.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.peluqueriacanina.persistencia;
+
+import com.mycompany.peluqueriacanina.logica.Duenio;
+import com.mycompany.peluqueriacanina.logica.Mascota;
+
+
+public class ControladoraPersistencia {
+    
+    DuenioJpaController duenioJpa = new DuenioJpaController();
+    MascotaJpaController mascoJpa = new MascotaJpaController();
+
+    public void guardar(Duenio duenio, Mascota masco) {
+        //creamos en la base de datos primero el dueño porque esta dentro de mascota
+       duenioJpa.create(duenio);
+       mascoJpa.create(masco);
+    }        
+}
